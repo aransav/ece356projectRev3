1e966f674eebd68c9ee0f1ecc73a26cf150a1d66
sivajega <sivajega@github.com>
2014-03-31 15:36:45 -0400

    1

--- proj/src/main/java/ca/uw/proj/controller/VisitationRecordController.java
+++ proj/src/main/java/ca/uw/proj/controller/VisitationRecordController.java
@@ -17,6 +17,7 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -65,19 +66,64 @@ public class VisitationRecordController {
         User u = (User) request.getSession().getAttribute("user");
         String role = (String) request.getSession().getAttribute("role");
 
+        String errMsg = "";
+
         if (request.getParameter("count") == null) {
             return visitListInfo(request);
         }
+
+        if (request.getParameter("errMsg") != null) {
+            errMsg = request.getParameter("errMsg");
+        }
+
         int id = Integer.valueOf(request.getParameter("count"));
 
         VisitationRecord v = vService.getAllVisitationRecord(Long.valueOf(id));
 
-        ModelAndView m = new ModelAndView();
+        ModelAndView m = new ModelAndView("visiation-record-info-update", "visiationRecordInfoUpdate", v);
         m.addObject("role", role);
         m.addObject("vrecord", v);
+        m.addObject("errMsg", errMsg);
 
-        m.setViewName("visiation-record-info-display");
-        
+        m.setViewName("visiation-record-info-update");
+
         return m;
+    }
+
+    @RequestMapping(value = "submitVRUpdate")
+    public ModelAndView submitVRUpdate(HttpServletRequest request, @ModelAttribute VisitationRecord _v) {
+        User u = (User) request.getSession().getAttribute("user");
+        String role = (String) request.getSession().getAttribute("role");
+
+        String errMessage;
+        ModelAndView m;
+        Long id = 0L;
+        VisitationRecord v;
+        try {
+            id = _v.getId();
+            v = vService.getAllVisitationRecord(id);
+
+            v.setComments(_v.getComments());
+            v.setDoctorPatient(_v.getDoctorPatient());
+            v.setDiagnosis(_v.getDiagnosis());
+            v.setEndTime(_v.getEndTime());
+            v.setRevNo(v.getRevNo()+1);
+            v.setStartTime(_v.getStartTime());
+            v.setSurgeryPerformed(_v.getSurgeryPerformed());
+            v.setVisitDate(_v.getVisitDate());
+            v.setVisitPrescription(_v.getVisitPrescription());
+            
+            vService.updateVisitationRecord(v);
+            
+        } catch (Exception e) {
+            errMessage = "Error during updating vistation record";
+            request.getSession().setAttribute("errMsg", errMessage);
+            request.getSession().setAttribute("count", id);
+            return visitationRecordInfoUpdate(request);
+        }
+
+        request.getSession().setAttribute("count", id);
+        return visitationRecordInfoUpdate(request);
+
     }
 }
